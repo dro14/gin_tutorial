7b066bb276f6a22c7a7cc22ec97d5118e48ff70e
dro14 <doniyorbek02@mail.ru>
2022-10-14 15:49:11 +0500

    complete video 2

--- video1/server.go
+++ video1/server.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"Desktop/gin_gonic/video1/controller"
+	"Desktop/gin_gonic/video1/middlewares"
 	"Desktop/gin_gonic/video1/service"
+	"io"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -12,8 +16,18 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
---
+++ video1/tester.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+  "net/http"
+  "io/ioutil"
+)
+
+func main() {
+
+  url := "http://localhost:8080/videos"
+  method := "GET"
+
+  payload := strings.NewReader(`{
+    "title": "Golang / Go Gin Framework Crash Course 02 | Middlewares 101: Authorization, Logging and Debugging",
+    "description": "In this video we are going to start working with Middlewares providing Authorization, Logging and Debugging capabilities to our API using Golang's Gin HTTP Framework.",
+    "url": "https://youtu.be/Ypwv1mFZ5vU"
+}`)
+
+  client := &http.Client {
+  }
+  req, err := http.NewRequest(method, url, payload)
+
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  req.Header.Add("Authorization", "Basic cHJhZ21hdGljOnJldmlld3M=")
+  req.Header.Add("Content-Type", "text/plain")
+
+  res, err := client.Do(req)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  defer res.Body.Close()
+
+  body, err := ioutil.ReadAll(res.Body)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println(string(body))
+}
